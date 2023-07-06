5414d0ab7d8e013a341e20276f185cc36dd3ec9e
gyurison7 <orangetree401@gmail.com>
2023-07-06 15:54:51 +0900

    input 작업

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { styled } from "styled-components";
 
 export const InputForm = styled.div`
@@ -27,18 +28,47 @@ export const SaveButton = styled.button`
 `
 
 const Input = () => {
+
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
+
+    const nameChangeHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    const priceChangeHandler = (event) => {
+        let price = event.target.value;
+        price = Number(price.replaceAll(',', '')); // 한글자 입력할때마다 price의 값이 바뀌어서 오기 때문에 isNan 검사전 ,떼고 Number로 바꿔줘야 함
+        if(isNaN(price)) {
+            setPrice(0);
+        } else {
+            setPrice(price.toLocaleString('ko-KR'));
+        }
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        if (name === '' || price === 0) {
+            alert("이름과 가격 모두 입력해주세요.");
+            return;
+        } else {
+            const removedPrice = price.replaceAll(',','');
+            alert(`{ name: ${name}, price: ${removedPrice} }`);
+        }
+    }
+
     return (
         <>
             <h1>Input</h1>
-            <form>
+            <form onSubmit={onSubmitHandler}>
                 <InputForm>
                     <div>
                         <label>이름</label>
-                        <InputComponents></InputComponents>
+                        <InputComponents type="text" value={name} onChange={nameChangeHandler} name="name"></InputComponents>
                     </div>
                     <div>
                         <label>가격</label>
-                        <InputComponents></InputComponents>
+                        <InputComponents type="text" value={price} onChange={priceChangeHandler} name="price"></InputComponents>
                     </div>
                     <SaveButton>저장</SaveButton>
                 </InputForm>
